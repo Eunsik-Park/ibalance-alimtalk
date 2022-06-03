21471977d237d28e9b7e3229106d8d31411d9a47
Eunsik Park <kongse92@gmail.com>
2022-06-03 18:06:15 +0900

    firebase login

---
+++ functions/index.js
@@ -0,0 +1,40 @@
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+const reportKey = "";
+const birthDate = "";
+const phoneNumber = "";
+const reportFile = "";
+
+//
+let requestAllimTalk = (reportKey, birthDate, phoneNumber, reportFile) => {
+
+    return;
+}
+
+//
+let populateFirebaseUser = (reportKey, birthDate) => {
+
+    return;
+}
+
+
+//
+let storeReport = (reportKey, birthDate) => {
+
+    return;
+}
+
+
+//
+let notifyToClient = (phoneNumber, reportKey) => {
+
+    return;
+}
