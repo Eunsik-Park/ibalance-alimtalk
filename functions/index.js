bd55acd15d216cc9bb2de0be949ae186a1ceacdc
Eunsik Park <kongse92@gmail.com>
2022-06-08 20:22:05 +0900

    create function populateFirebaseUser for creating new user

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,40 @@
+
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp({credential: admin.credential.applicationDefault()});
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-const reportKey = "";
-const birthDate = "";
-const phoneNumber = "";
-const reportFile = "";
+exports.populateFirebaseUser = functions.region('asia-northeast3').https.onRequest((request, response) => {
+  ({reportKey, birthDate} = request.body);
+  request.get('content-type')
+  //functions.logger.info("Hello logs!", {structuredData: true});
+  response.send(populateFirebaseUser(`${reportKey}`, `${birthDate}`));
+});
 
-//
+
+let populateFirebaseUser = (reportKey, birthDate) => {  
+  admin.auth().createUser({
+    email: reportKey+'@teamelysium.kr',
+    password: birthDate
+  })
+  .then((userRecord) => {
+    // See the UserRecord reference doc for the contents of userRecord.
+    console.log('Successfully created new user and reportKey :', userRecord.id);
+  })
+  .catch((error) => {
+    console.log('Error creating new user:', error);
+  });
+}
+
+
 let requestAllimTalk = (reportKey, birthDate, phoneNumber, reportFile) => {
 
+  
+   
     return;
 }
 
-//
-let populateFirebaseUser = (reportKey, birthDate) => {
-
-    return;
-}
 
 
 //
