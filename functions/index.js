00ca14c26c1d755054707c6b3ea231248957edf3
Eunsik Park <kongse92@gmail.com>
2022-06-09 21:33:14 +0900

    edit type for modular on package.json

--- functions/index.js
+++ functions/index.js
@@ -1,51 +1,91 @@
+// import {firebase} from "firebase";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp({credential: admin.credential.applicationDefault()});
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAADchBOBr9crcF2MWGxk_GQrJlUToIHp4",
+  authDomain: "ibalance-alimtalk.firebaseapp.com",
+  projectId: "ibalance-alimtalk",
+  storageBucket: "ibalance-alimtalk.appspot.com",
+  messagingSenderId: "142352845326",
+  appId: "1:142352845326:web:aaaa71633f132cf7c78a5f"
+};
+// const functions = require("firebase-functions");
+// const admin = require("firebase-admin");
 
-// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+const auth = getAuth();
+// initialize on Cloud Functions
+const firebaseApp = initializeApp(firebaseConfig);
+// get a refer to the storage service
+const storage = getStorage(firebaseApp);
 
-exports.populateFirebaseUser = functions.region('asia-northeast3').https.onRequest((request, response) => {
+
+exports.getBirthDate = functions.region('asia-northeast3').https.onRequest((request, response) => {
   ({reportKey, birthDate} = request.body);
-  request.get('content-type')
-  //functions.logger.info("Hello logs!", {structuredData: true});
-  response.send(populateFirebaseUser(`${reportKey}`, `${birthDate}`));
+  // ({data} = request.data); 
+  request.get('content-type', 'application/json');
+
+  if(reportKey || reportKey !== 'undefined'){
+    populateFirebaseUser(`${reportKey}`, `${birthDate}`).then((receiveId) => {
+      // const uid = receiveId;
+      // storeReport(receiveId); 
+    });
+    response.send(`reponse Data : {reportKey : ${reportKey} birthDate : ${birthDate}}`);
+    
+    
+    //TODO : Use this request.data for uploading images.
+    
+    // console.log("request.data : " + `${data}`);
+    //storeReport(uid);
+
+  }else{
+    console.log("Please check birthDate");
+  }
+
 });
 
 
-let populateFirebaseUser = (reportKey, birthDate) => {  
-  admin.auth().createUser({
+
+const requestAlimTalk = (reportKey, birthDate, phoneNumber, reportFile) => {
+  
+}
+
+const populateFirebaseUser = async(reportKey, birthDate) => {
+  return getAuth().createUser({
     email: reportKey+'@teamelysium.kr',
+    emailVerified: false,
     password: birthDate
   })
   .then((userRecord) => {
-    // See the UserRecord reference doc for the contents of userRecord.
-    console.log('Successfully created new user and reportKey :', userRecord.id);
+    return userRecord.uid;
   })
   .catch((error) => {
-    console.log('Error creating new user:', error);
+    const errorCode = error.code;
+    switch(errorCode){
+      case 'auth/invalid-email':
+        // TODO : need to edit later
+        console.log("auth/invalid-email.");
+        case 'auth/email-already-exists':
+          // TODO : need to edit later
+          console.log("auth/email-already-exists.");
+        }
+
+
+    const errorMessage = error.message;
   });
 }
 
+const storeReport = (uid)=> {  
+  // console.log("storage : " + storage);
+  // const storageReference = storageBucket.ref();
+  for(proper in storageReference){
+    console.log("storage data : " + proper);
+  }
+  //storageReference.child('images/mountains.jpg');
+} 
 
-let requestAllimTalk = (reportKey, birthDate, phoneNumber, reportFile) => {
-
-  
-   
-    return;
-}
-
-
-
-//
-let storeReport = (reportKey, birthDate) => {
-
-    return;
-}
-
-
-//
 let notifyToClient = (phoneNumber, reportKey) => {
 
-    return;
 }
